b3169a2339e3d536773a545a8de7a02412f2bc02
Dante Arrighi <dantearrighi@gmail.com>
2024-02-10 12:39:17 -0300

    Install OpenAI and configure openai for orthography use-case

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 import { OrthographyDto } from './dtos';
 import { orthographyCheckUseCase } from './use-cases';
 
+// Will be used ONLY to call use-cases
 @Injectable()
 export class GptService {
-  // Will be used ONLY to call use-cases
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckUseCase({
+    return await orthographyCheckUseCase(this.openai, {
       prompt: orthographyDto.prompt,
     });
   }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,12 +1,51 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
 
-  return {
-    prompt: prompt,
-    apikey: process.env.OPENAI_API_KEY,
-  };
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: `
+        You're an assistant.
+        You will receive texts in spanish or english with possible orthographic and grammatical errors. 
+        The words should exists in the language dictionary (like Real academia española
+        or Oxford dictionary).
+        You must respond in JSON format,
+        Your task is to correct the errors and return the corrected text.
+        You also must to give a percentege of accuracy of the user's text.
+
+        If there's no errors, you will return a congratulations message.
+
+        Output example:
+        {
+          userScore: number,
+          errors: string[], // ['error -> solution']
+          message: string, // Use emojis and text to congratulate the user
+        }
+        `,
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    model: 'gpt-3.5-turbo-1106',
+    temperature: 0.3,
+    max_tokens: 150,
+    response_format: {
+      type: 'json_object',
+    },
+  });
+
+  const jsonResp = JSON.parse(completion.choices[0].message.content);
+  return jsonResp;
 };
